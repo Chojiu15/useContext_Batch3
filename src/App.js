75e23faafd5edfa8fcdf38ce4e76eeba44340d68
Chojiu <dev.cyth@gmail.com>
2022-01-19 17:36:45 +0100

    initial useContext commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {MovieController} from './context/MoviesContext'
+import NavBar from './components/NavBar'
+import MovieList from './components/MovieList';
+import AddMovie from './components/AddMovie';
+import {ApiController} from './context/ApiContext'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <MovieController>
+          <ApiController>
+          <NavBar />
+          <MovieList />
+          <AddMovie />
+          </ApiController>
+      </MovieController>
     </div>
   );
 }
